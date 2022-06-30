05d699589edb38d0034c7d5318844630033c4312
ankanroy-code <8ankanroy@gmail.com>
2022-06-30 15:33:20 +0530

    update readme
    Signed-off-by: ankanroy-code <8ankanroy@gmail.com>

---
+++ components/Blog.tsx
@@ -0,0 +1,11 @@
+import { GetStaticProps} from "next";
+
+export const getStaticProps = () : GetStaticProps => {
+  return (
+  props : { }
+  )
+}
+
+export const Blog = (props : { }): void => {
+  console.log("DEBUG : " + props)
+};
